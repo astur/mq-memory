892cda2f9a098cf06a66cd0fa1cf2b41d12a9b0f
astur <astur@yandex.ru>
2018-06-28 23:49:30 +0300

    api stub

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-module.exports = 'under construction';
+module.exports = (db, {
+    name = 'mq',
+    ttl = 30000,
+    tries = 10,
+    insistent = false,
+    items = null,
+    strict = false,
+} = {}) => {
+    const total = () => {};
+
+    const waiting = () => {};
+
+    const active = () => {};
+
+    const failed = () => {};
+
+    const stats = () => {};
+
+    const add = () => {};
+
+    const get = () => {};
+
+    const ack = () => {};
+
+    const ping = () => {};
+
+    return {
+        add,
+        get,
+        ack,
+        ping,
+        total,
+        waiting,
+        active,
+        failed,
+        stats,
+
+        get options(){
+            return {ttl, tries, insistent};
+        },
+    };
+};

5e5b2b1051a635f0ec839d90bc302003b3b7a028
astur <astur@yandex.ru>
2018-07-05 00:07:34 +0300

    todos for tests

--- test.js
+++ test.js
@@ -5,3 +5,17 @@ test('stub', t => {
     t.pass();
     t.is(mq, mq);
 });
+
+test.todo('add');
+test.todo('get');
+test.todo('ack');
+test.todo('ping');
+test.todo('tries');
+test.todo('ttl');
+test.todo('null-trie');
+test.todo('insistent');
+test.todo('size tries');
+test.todo('size no-tries');
+test.todo('init items');
+test.todo('queue options');
+test.todo('get strict');
